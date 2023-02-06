39602bf73bf18f93eeb7c44e9b247b3a77628f1a
adityarizkyramadhan <90701908+adityarizkyramadhan@users.noreply.github.com>
2023-02-06 20:52:43 +0700

    add configuration

--- config/db_conn.go
+++ config/db_conn.go
@@ -1,1 +1,25 @@
 package config
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func MakeConnectionDatabase(data Database) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("user=%s "+
+		"password=%s "+
+		"host=%s "+
+		"TimeZone=Asia/Singapore "+
+		"port=%s "+
+		"dbname=%s", data.SupabaseUser, data.SupabasePassword, data.SupabaseHost, data.SupabasePort, data.SupabaseDbName)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	if err := db.AutoMigrate(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
---
+++ config/db_driver.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"os"
+
+	validation "github.com/go-ozzo/ozzo-validation"
+)
+
+type Database struct {
+	SupabaseUser     string
+	SupabasePassword string
+	SupabaseHost     string
+	SupabasePort     string
+	SupabaseDbName   string
+}
+
+func NewDatabase() (*Database, error) {
+	var err error
+	cfgDb := Database{
+		SupabaseUser:     os.Getenv("SUPABASE_USER"),
+		SupabasePassword: os.Getenv("SUPABASE_PASSWORD"),
+		SupabaseHost:     os.Getenv("SUPABASE_HOST"),
+		SupabasePort:     os.Getenv("SUPABASE_PORT"),
+		SupabaseDbName:   os.Getenv("SUPABASE_DB_NAME"),
+	}
+	err = cfgDb.validate()
+	if err != nil {
+		return nil, err
+	}
+	return &cfgDb, err
+}
+
+func (db Database) validate() error {
+	return validation.ValidateStruct(
+		&db,
+		validation.Field(&db.SupabaseUser, validation.Required),
+		validation.Field(&db.SupabasePassword, validation.Required),
+		validation.Field(&db.SupabaseHost, validation.Required),
+		validation.Field(&db.SupabasePort, validation.Required),
+		validation.Field(&db.SupabaseDbName, validation.Required),
+	)
+}
